57ae72bf8159dcfd5dce4860d331cc7a89078d5a
mcleanka <mcleankasambala@gmail.com>
2026-08-25 12:05:29 +0200

    Add ascending/descending sort toggle to DistrictPicker

--- src/pickers/DistrictPicker.js
+++ src/pickers/DistrictPicker.js
@@ -3,7 +3,11 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { injectIntl } from "react-intl";
 import _debounce from "lodash/debounce";
+import _ from "lodash";
 import { withTheme, withStyles } from "@material-ui/core/styles";
+import { IconButton, Tooltip } from "@material-ui/core";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import { withModulesManager, formatMessage, AutoSuggestion } from "@openimis/fe-core";
 import { selectDistrictLocation, clearLocations } from "../actions.js";
 import { locationLabel } from "../utils";
@@ -12,6 +16,14 @@ const styles = (theme) => ({
   textField: {
     width: "100%",
   },
+  pickerRow: {
+    display: "flex",
+    alignItems: "flex-end",
+  },
+  pickerField: {
+    flex: 1,
+    minWidth: 0,
+  },
 });
 
 class DistrictPicker extends Component {
@@ -19,6 +31,14 @@ class DistrictPicker extends Component {
     super(props);
     this.selectThreshold = props.modulesManager.getConf("fe-location", "DistrictPicker.selectThreshold", 10);
   }
+
+  state = {
+    sortAsc: true,
+  };
+
+  toggleSort = () => {
+    this.setState((state) => ({ sortAsc: !state.sortAsc }));
+  };
 
   onSuggestionSelected = (v) => {
     if (v && this.props.value !== v) this.props.selectDistrictLocation(v);
@@ -32,6 +52,7 @@ class DistrictPicker extends Component {
   render() {
     const {
       intl,
+      classes,
       userHealthFacilityFullPath,
       reset,
       value,
@@ -46,38 +67,57 @@ class DistrictPicker extends Component {
       required = false,
       title,
     } = this.props;
+    const { sortAsc } = this.state;
 
     let items = userHealthFacilityFullPath && [userHealthFacilityFullPath.location] || districts || [];
-    
+
     if (!!region) {
       items = items.filter((d) => {
         return d.parent.uuid === region.uuid;
       });
     }
+    items = _.orderBy(items, ["name"], [sortAsc ? "asc" : "desc"]);
 
     return (
-      <AutoSuggestion
-        module="location"
-        items={items}
-        label={!!withLabel && (label || formatMessage(intl, "location", "DistrictPicker.label"))}
-        lookup={locationLabel}
-        getSuggestionValue={locationLabel}
-        renderSuggestion={(a) => <span>{locationLabel(a)}</span>}
-        onSuggestionSelected={this.onSuggestionSelected}
-        onClear={this.onSuggestionSelected}
-        value={value}
-        reset={reset}
-        readOnly={readOnly}
-        required={required}
-        selectThreshold={this.selectThreshold}
-        withNull={withNull}
-        nullLabel={
-          nullLabel || filterLabels
-            ? formatMessage(intl, "location", "location.DistrictPicker.null")
-            : formatMessage(intl, "location", "location.DistrictPicker.none")
-        }
-        title={title}
-      />
+      <div className={classes.pickerRow}>
+        <div className={classes.pickerField}>
+          <AutoSuggestion
+            module="location"
+            items={items}
+            label={!!withLabel && (label || formatMessage(intl, "location", "DistrictPicker.label"))}
+            lookup={locationLabel}
+            getSuggestionValue={locationLabel}
+            renderSuggestion={(a) => <span>{locationLabel(a)}</span>}
+            onSuggestionSelected={this.onSuggestionSelected}
+            onClear={this.onSuggestionSelected}
+            value={value}
+            reset={reset}
+            readOnly={readOnly}
+            required={required}
+            selectThreshold={this.selectThreshold}
+            withNull={withNull}
+            nullLabel={
+              nullLabel || filterLabels
+                ? formatMessage(intl, "location", "location.DistrictPicker.null")
+                : formatMessage(intl, "location", "location.DistrictPicker.none")
+            }
+            title={title}
+          />
+        </div>
+        {!readOnly && (
+          <Tooltip
+            title={formatMessage(
+              intl,
+              "location",
+              sortAsc ? "location.DistrictPicker.sortDescending" : "location.DistrictPicker.sortAscending",
+            )}
+          >
+            <IconButton size="small" onClick={this.toggleSort}>
+              {sortAsc ? <ArrowUpwardIcon fontSize="small" /> : <ArrowDownwardIcon fontSize="small" />}
+            </IconButton>
+          </Tooltip>
+        )}
+      </div>
     );
   }
 }
